8423e13cfd064a48cb539f36b2d848c747866250
unknown <brunotx1987@gmail.com>
2020-05-12 16:47:45 -0300

    change package

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + '/dist/lookAfterApp'));
+
+app.get('/*', (req, res) => {
+    res.sendFile(__dirname + '/dist/lookAfterApp/index.html');
+});
+
+app.listen(PORT, () => {
+    console.log('Server online...')
+});
