76f64fbb619cd1232e6d32ba554d871ace3d5197
unknown <brunotx1987@gmail.com>
2020-05-13 17:20:32 -0300

    add ngxbootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,24 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './routes/pages/home/home.component';
 import { CarrouselComponent } from './routes/pages/carrousel/carrousel.component';
 import { HttpClientModule } from '@angular/common/http';
+import { UserAddComponent } from './routes/pages/user-add/user-add.component';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    CarrouselComponent
+    CarrouselComponent,
+    UserAddComponent
   ],
   imports: [
     HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ModalModule.forRoot()
   ],
   providers: [],
+  entryComponents: [UserAddComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
