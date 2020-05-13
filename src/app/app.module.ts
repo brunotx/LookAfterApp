817f270d0ac0434d25d1e4a06ed19653279941c7
unknown <brunotx1987@gmail.com>
2020-05-13 12:14:31 -0300

    carrousel not finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './routes/app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './routes/pages/home/home.component';
+import { CarrouselComponent } from './routes/pages/carrousel/carrousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CarrouselComponent
   ],
   imports: [
     BrowserModule,
